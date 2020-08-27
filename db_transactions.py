04418ce859b9f85050cda0b22f14ad64cdda70a4
Vidalatan <59946423+Vidalatan@users.noreply.github.com>
2020-08-27 16:58:16 -0500

    Added db_transactions.py
    Added db_transactions.py to repository and includes the main functions of interacting with the database

---
+++ db_transactions.py
@@ -0,0 +1,22 @@
+import os
+
+def upload_player(player):
+    with open('player_db.txt', 'a') as db:
+        string = player.name + "|" + player.password + "|"
+        now = ",".join(player.resources)
+        string += (now+"|")
+        now = ",".join(player.products)
+        string += now
+        db.write(string+"\n")
+
+def download_player(player,password):
+    with open('player_db.txt') as db:
+        for x in db:
+            if player in x:
+                if player+"|"+password in x:
+                    info = x.split("|")
+                    return info
+                else:
+                    return "Password failed to match"
+            else:
+                return "Username failed to match"
